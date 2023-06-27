417336df756fd8301b35557e74186b6e5d2b2755
utkarshsrivastava2508 <76029943+utkarshsrivastava2508@users.noreply.github.com>
2023-06-27 15:50:47 +0530

    Create pyspark_map_to_col_to_map.py

---
+++ pyspark_map_to_col_to_map.py
@@ -0,0 +1,66 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+spark = SparkSession.builder.appName('pyspark_examples').getOrCreate()
+
+dataDictionary = [
+        ('James',{'hair':'black','eye':'brown'}),
+        ('Michael',{'hair':'brown','eye':None}),
+        ('Robert',{'hair':'red','eye':'black'}),
+        ('Washington',{'hair':'grey','eye':'grey'}),
+        ('Jefferson',{'hair':'brown','eye':''})
+        ]
+
+df = spark.createDataFrame(data=dataDictionary, schema = ['name','properties'])
+df.printSchema()
+df.show(truncate=False)
+
+df3=df.rdd.map(lambda x: \
+    (x.name,x.properties["hair"],x.properties["eye"])) \
+    .toDF(["name","hair","eye"])
+df3.printSchema()
+df3.show()
+
+df.withColumn("hair",df.properties.getItem("hair")) \
+  .withColumn("eye",df.properties.getItem("eye")) \
+  .drop("properties") \
+  .show()
+
+df.withColumn("hair",df.properties["hair"]) \
+  .withColumn("eye",df.properties["eye"]) \
+  .drop("properties") \
+  .show()
+
+# Functions
+from pyspark.sql.functions import explode,map_keys,col
+
+keysDF = df.select(explode(map_keys(df.properties))).distinct()
+keysList = keysDF.rdd.map(lambda x:x[0]).collect()
+keyCols = list(map(lambda x: col("properties").getItem(x).alias(str(x)), keysList))
+df.select(df.name, *keyCols).show()
+
+
+
+data = [ ("36636","Finance",3000,"USA"), 
+    ("40288","Finance",5000,"IND"), 
+    ("42114","Sales",3900,"USA"), 
+    ("39192","Marketing",2500,"CAN"), 
+    ("34534","Sales",6500,"USA") ]
+schema = StructType([
+     StructField('id', StringType(), True),
+     StructField('dept', StringType(), True),
+     StructField('salary', IntegerType(), True),
+     StructField('location', StringType(), True)
+     ])
+
+df = spark.createDataFrame(data=data,schema=schema)
+df.printSchema()
+df.show(truncate=False)
+
+#Convert scolumns to Map
+from pyspark.sql.functions import col,lit,create_map
+df = df.withColumn("propertiesMap",create_map(
+        lit("salary"),col("salary"),
+        lit("location"),col("location")
+        )).drop("salary","location")
+df.printSchema()
+df.show(truncate=False)
